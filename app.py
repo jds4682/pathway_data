1cbb6b90bb174b908fde702943289dc4459adc42
jds4682 <41563562+jds4682@users.noreply.github.com>
2025-03-25 15:47:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,83 @@ def load_pathway_data(name):
         return None
 
 # Define filters
+T1 = ["Daeshiho-tang", "SMHB00058", "SMHB00188", "SMHB00021", "SMHB00085", "SMHB00486", "SMHB00035"]
+T2 = ["Banhabakchulcheonmatang", "SMHB00035", "SMHB00064", "SMHB00272", "SMHB00022", "SMHB00052", "SMHB00336", "SMHB00187", "SMHB00527", "SMHB00129", "SMHB00480", "SMHB00136", "SMHB00184", "SMHB00367"]
+T3 = ["Banhasasim-tang", "SMHB00035", "SMHB00188", "SMHB00336", "SMHB00133", "SMHB00136", "SMHB00186", "SMHB00367", "SMHB00090"]
+T4 = ["Banhahubak-tang", "SMHB00035", "SMHB00129", "SMHB00171", "SMHB00497", "SMHB00367", "SMHB00090"]
+T5 = ["Bojungikgi-tang", "SMHB00187", "SMHB00336", "SMHB00022", "SMHB00133", "SMHB00095", "SMHB00064", "SMHB00366", "SMHB00058"]
 T6 = ["Saengmaek-san", "SMHB00336", "SMHB00041"]
-T6_weights = {"SMHB00336": 3.75, "SMHB00041": 3.75}
-
 T7 = ["Sosiho-tang", "SMHB00058", "SMHB00188", "SMHB00336", "SMHB00035", "SMHB00133", "SMHB00367", "SMHB00090"]
+T8 = ["Socheongryong-tang", "SMHB00264", "SMHB00021", "SMHB00035", "SMHB00041", "SMHB00155", "SMHB00425", "SMHB00133", "SMHB00136"]
+T9 = ["Hyeonggaeyeongyo-tang", "SMHB00212", "SMHB00241", "SMHB00122", "SMHB00095", "SMHB00077", "SMHB00058", "SMHB00485", "SMHB00188", "SMHB00027", "SMHB00216", "SMHB00021", "SMHB00483", "SMHB00133"]
+T10 = ["Hwanglyeonhaedok-tang", "SMHB00186", "SMHB00188", "SMHB00184", "SMHB00483"]
+T11 = ["Gamisoyo-san", "SMHB00483", "SMHB00284", "SMHB00133", "SMHB00129", "SMHB00095", "SMHB00058", "SMHB00037", "SMHB00022", "SMHB00021"]
+T12 = ["Galgeun-tang", "SMHB00021", "SMHB00090", "SMHB00133", "SMHB00155", "SMHB00264", "SMHB00367", "SMHB00499"]
+T13 = ["Galgeunhaegi-tang", "SMHB00021", "SMHB00027", "SMHB00058", "SMHB00090", "SMHB00133", "SMHB00188", "SMHB00216", "SMHB00322", "SMHB00366", "SMHB00367", "SMHB00499"]
+
+
+T1_weights = {
+    "SMHB00058": 15,
+    "SMHB00188": 9.38,
+    "SMHB00021": 9.38,
+    "SMHB00085": 7.5,
+    "SMHB00486": 5.63,
+    "SMHB00035": 3.75
+}
+
+T2_weights = {
+    "SMHB00035": 5.63,
+    "SMHB00064": 5.63,
+    "SMHB00272": 5.63,
+    "SMHB00022": 3.75,
+    "SMHB00052": 1.88,
+    "SMHB00336": 1.88,
+    "SMHB00187": 1.88,
+    "SMHB00527": 1.88,
+    "SMHB00129": 1.88,
+    "SMHB00480": 1.88,
+    "SMHB00136": 1.13,
+    "SMHB00184": 0.75,
+    "SMHB00367": 2.5
+}
+
+T3_weights = {
+    "SMHB00035": 7.5,
+    "SMHB00188": 5.63,
+    "SMHB00336": 5.63,
+    "SMHB00133": 5.63,
+    "SMHB00136": 3.75,
+    "SMHB00186": 1.88,
+    "SMHB00367": 1.5,
+    "SMHB00090": 2
+}
+
+T4_weights = {
+    "SMHB00035": 7.5,
+    "SMHB00129": 6,
+    "SMHB00171": 4.5,
+    "SMHB00497": 3,
+    "SMHB00367": 3.5,
+    "SMHB00090": 2
+}
+
+T5_weights = {
+    "SMHB00187": 5.63,
+    "SMHB00336": 3.75,
+    "SMHB00022": 3.75,
+    "SMHB00133": 3.75,
+    "SMHB00095": 1.88,
+    "SMHB00064": 1.88,
+    "SMHB00366": 1.13,
+    "SMHB00058": 1.13
+}
+
+T6_weights = {
+    
+    "SMHB00336" : 3.75,
+    "SMHB00041" : 3.75 
+}
+
 T7_weights = {
     "SMHB00058": 11.25,
     "SMHB00188": 7.5,
@@ -31,10 +104,95 @@ T7_weights = {
     "SMHB00367": 1.5,
     "SMHB00090": 2
 }
+
+T8_weights = {
+    "SMHB00264": 5.63,
+    "SMHB00021": 5.63,
+    "SMHB00035": 5.63,
+    "SMHB00041": 5.63,
+    "SMHB00155": 3.75,
+    "SMHB00425": 3.75,
+    "SMHB00133": 3.75,
+    "SMHB00136": 3.75
+}
+
+T9_weights = {
+    "SMHB00212": 2.63,
+    "SMHB00241": 2.63,
+    "SMHB00122": 2.63,
+    "SMHB00095": 2.63,
+    "SMHB00077": 2.63,
+    "SMHB00058": 2.63,
+    "SMHB00485": 2.63,
+    "SMHB00188": 2.63,
+    "SMHB00027": 2.63,
+    "SMHB00216": 2.63,
+    "SMHB00021": 2.63,
+    "SMHB00483": 2.63,
+    "SMHB00133": 1.88
+}
+
+T10_weights = {
+    "SMHB00186": 4.69,
+    "SMHB00188": 4.69,
+    "SMHB00184": 4.69,
+    "SMHB00483": 4.69
+}
+
+T11_weights = {
+    "SMHB00095": 3.75,
+    "SMHB00021": 3.75,
+    "SMHB00129": 3.75,
+    "SMHB00022": 3.75,
+    "SMHB00058": 3.75,
+    "SMHB00483": 3.75,
+    "SMHB00284": 3.75,
+    "SMHB00133": 3.75,
+    "SMHB00037": 1.88
+}
+
+T12_weights = {
+    "SMHB00499": 11.25,
+    "SMHB00264": 7.5,
+    "SMHB00021": 5.63,
+    "SMHB00155": 3.75,
+    "SMHB00133": 3,
+    "SMHB00367": 1.5,
+    "SMHB00090": 2
+}
+
+T13_weights = {
+    "SMHB00499": 3.75,
+    "SMHB00058": 3.75,
+    "SMHB00188": 3.75,
+    "SMHB00322": 3.75,
+    "SMHB00542": 3.75,
+    "SMHB00021": 3.75,
+    "SMHB00366": 3.75,
+    "SMHB00027": 3.75,
+    "SMHB00216": 3.75,
+    "SMHB00133": 1.88,
+    "SMHB00367": 1.5,
+    "SMHB00090": 2
+}
 #don't touch above this area!!!!!!!!!!!!!
 
 
-filters = {T6[0]: (T6, T6_weights), T7[0]: (T7, T7_weights)}
+filters = {
+    T1[0]: (T1, T1_weights),
+    T2[0]: (T2, T2_weights),
+    T3[0]: (T3, T3_weights),
+    T4[0]: (T4, T4_weights),
+    T5[0]: (T5, T5_weights),
+    T6[0]: (T6, T6_weights),
+    T7[0]: (T7, T7_weights),
+    T8[0]: (T8, T8_weights),
+    T9[0]: (T9, T9_weights),
+    T10[0]: (T10, T10_weights),
+    T11[0]: (T11, T11_weights),
+    T12[0]: (T12, T12_weights),
+    T13[0]: (T13, T13_weights),
+}
 filter_options = list(filters.keys())
 selected_filter = st.selectbox("Select a Filter", filter_options)
 selected_tang, selected_weights = filters[selected_filter]
